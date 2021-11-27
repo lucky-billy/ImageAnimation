71bb70f7a40af29e9ea478864f1ec2f220662c79
QY-Admin <1623812580@qq.com>
2021-11-27 16:17:54 +0800

    Add blinds

--- Content/Blinds.qml
+++ Content/Blinds.qml
@@ -1,38 +1,116 @@
-﻿import QtQuick 2.0
+﻿import QtQuick 2.12
+import "./function.js" as JSFunction
 
 // 百叶窗
 Rectangle {
     color: "#858585"
 
-    Image {
-        id: sourceImage1
+    Column {
         x: 20; y: 20
-        sourceSize: Qt.size(480, 270)
-        source: "file:///D:/Qt/ImageAnimation/Image/1.jpg"
-        smooth: true
-    }
+        spacing: 20
 
-    Image {
-        id: sourceImage2
-        x: 520; y: 20
-        sourceSize: Qt.size(480, 270)
-        source: "file:///D:/Qt/ImageAnimation/Image/1.jpg"
-        smooth: true
-    }
+        Row {
+            spacing: 20
 
-    Image {
-        id: sourceImage3
-        x: 20; y: 310
-        sourceSize: Qt.size(480, 270)
-        source: "file:///D:/Qt/ImageAnimation/Image/1.jpg"
-        smooth: true
-    }
+            ShaderEffect {
+                width: 480; height: 270
 
-    Image {
-        id: sourceImage4
-        x: 520; y: 310
-        sourceSize: Qt.size(480, 270)
-        source: "file:///D:/Qt/ImageAnimation/Image/1.jpg"
-        smooth: true
+                property int rows: 10       // 行数
+                property int cols: 10       // 列数
+                property int percent: 0     // 百分比
+
+                // 图像
+                property ShaderEffectSource effectSource: ShaderEffectSource {
+                    hideSource: true
+                    sourceItem: Image {
+                        sourceSize: Qt.size(480, 270)
+                        source: "file:///D:/Qt/ImageAnimation/Image/1.jpg"
+                        fillMode: Image.Tile
+                    }
+                }
+
+                // 动画 - 通过控制百分比实现特效
+                NumberAnimation on percent { from: 0; to: 100; duration: 1000; running: true }
+
+                // 片段着色器
+                fragmentShader: JSFunction.getShader("Blinds_fragmentShader_1")
+            }
+
+            ShaderEffect {
+                width: 480; height: 270
+
+                property int rows: 8        // 行数
+                property int cols: 8        // 列数
+                property int percent: 0     // 百分比
+
+                // 图像
+                property ShaderEffectSource effectSource: ShaderEffectSource {
+                    hideSource: true
+                    sourceItem: Image {
+                        sourceSize: Qt.size(480, 270)
+                        source: "file:///D:/Qt/ImageAnimation/Image/1.jpg"
+                        fillMode: Image.Tile
+                    }
+                }
+
+                // 动画 - 通过控制百分比实现特效
+                NumberAnimation on percent { from: 0; to: 100; duration: 1000; running: true }
+
+                // 片段着色器
+                fragmentShader: JSFunction.getShader("Blinds_fragmentShader_2")
+            }
+        }
+
+        Row {
+            spacing: 20
+
+            ShaderEffect {
+                width: 480; height: 270
+
+                property int rows: 10       // 行数
+                property int cols: 10       // 列数
+                property int percent: 0     // 百分比
+
+                // 图像
+                property ShaderEffectSource effectSource: ShaderEffectSource {
+                    hideSource: true
+                    sourceItem: Image {
+                        sourceSize: Qt.size(480, 270)
+                        source: "file:///D:/Qt/ImageAnimation/Image/1.jpg"
+                        fillMode: Image.Tile
+                    }
+                }
+
+                // 动画 - 通过控制百分比实现特效
+                NumberAnimation on percent { from: 0; to: 100; duration: 1000; running: true }
+
+                // 片段着色器
+                fragmentShader: JSFunction.getShader("Blinds_fragmentShader_3")
+            }
+
+            ShaderEffect {
+                width: 480; height: 270
+
+                property int rows: 8        // 行数
+                property int cols: 8        // 列数
+                property int percent: 0     // 百分比
+
+                // 图像
+                property ShaderEffectSource effectSource: ShaderEffectSource {
+                    hideSource: true
+                    sourceItem: Image {
+                        sourceSize: Qt.size(480, 270)
+                        source: "file:///D:/Qt/ImageAnimation/Image/1.jpg"
+                        fillMode: Image.Tile
+                    }
+                }
+
+                // 动画 - 通过控制百分比实现特效
+                NumberAnimation on percent { from: 0; to: 100; duration: 1000; running: true }
+
+                // 片段着色器
+                fragmentShader: JSFunction.getShader("Blinds_fragmentShader_4")
+            }
+        }
     }
 }
---
+++ Content/function.js
@@ -0,0 +1,167 @@
+﻿function getShader(type) {
+    var ret
+
+    // 百叶窗 - 从左往右
+    if ( type === "Blinds_fragmentShader_1" )
+    {
+        ret = "\
+        varying vec2 qt_TexCoord0;          // 共享纹理坐标
+        uniform float qt_Opacity;           // 透明度
+        uniform sampler2D effectSource;     // 2D 纹理，即上面定义的 effectSource
+
+        uniform int rows;
+        uniform int cols;
+        uniform int percent;
+
+        void main()
+        {
+            vec4 color = texture2D(effectSource, qt_TexCoord0);
+
+            int xlen = 100 / cols;
+            int ylen = 100 / rows;
+
+            float p = float(percent) / 100.0;
+            int x = int(qt_TexCoord0.x * 100.0);
+            int y = int(qt_TexCoord0.y * 100.0);
+            int sx = x / xlen;
+            int sy = y / ylen;
+
+            int xOffset = 0;
+            if ( sy % 2 != 0 ) {
+                xOffset = xlen / 2;
+            }
+
+            float alpha = 0.0;
+            if ( sx * xlen <= x + xOffset && (x + xOffset) % xlen <= int(float(xlen) * p) ) {
+                alpha = 1.0;
+            }
+
+            alpha *= qt_Opacity;
+            gl_FragColor = vec4(color.rgb * alpha, alpha);
+        }"
+    }
+
+    // 百叶窗 - 从右往左
+    else if ( type === "Blinds_fragmentShader_2" )
+    {
+        ret = "\
+        varying vec2 qt_TexCoord0;          // 共享纹理坐标
+        uniform float qt_Opacity;           // 透明度
+        uniform sampler2D effectSource;     // 2D 纹理，即上面定义的 effectSource
+
+        uniform int rows;
+        uniform int cols;
+        uniform int percent;
+
+        void main()
+        {
+            vec4 color = texture2D(effectSource, qt_TexCoord0);
+
+            int xlen = 100 / cols;
+            int ylen = 100 / rows;
+
+            float p = float(percent) / 100.0;
+            int x = int(qt_TexCoord0.x * 100.0);
+            int y = int(qt_TexCoord0.y * 100.0);
+            int sx = x / xlen;
+            int sy = y / ylen;
+
+            int xOffset = 0;
+            if ( sy % 2 != 0 ) {
+                xOffset = xlen / 2;
+            }
+
+            float alpha = 0.0;
+            if ( sx * xlen <= x + xOffset && (x + xOffset) % xlen <= int(float(xlen) * p) ) {
+                alpha = 1.0;
+            }
+
+            alpha *= qt_Opacity;
+            gl_FragColor = vec4(color.rgb * alpha, alpha);
+        }"
+    }
+
+    // 百叶窗 - 从上往下
+    else if ( type === "Blinds_fragmentShader_3" )
+    {
+        ret = "\
+        varying vec2 qt_TexCoord0;          // 共享纹理坐标
+        uniform float qt_Opacity;           // 透明度
+        uniform sampler2D effectSource;     // 2D 纹理，即上面定义的 effectSource
+
+        uniform int rows;
+        uniform int cols;
+        uniform int percent;
+
+        void main()
+        {
+            vec4 color = texture2D(effectSource, qt_TexCoord0);
+
+            int xlen = 100 / cols;
+            int ylen = 100 / rows;
+
+            float p = float(percent) / 100.0;
+            int x = int(qt_TexCoord0.x * 100.0);
+            int y = int(qt_TexCoord0.y * 100.0);
+            int sx = x / xlen;
+            int sy = y / ylen;
+
+            int yOffset = 0;
+            if ( sx % 2 != 0 ) {
+                yOffset = ylen / 2;
+            }
+
+            float alpha = 0.0;
+            if ( sy * ylen <= y + yOffset && (y + yOffset) % ylen <= int(float(ylen) * p) ) {
+                alpha = 1.0;
+            }
+
+            alpha *= qt_Opacity;
+            gl_FragColor = vec4(color.rgb * alpha, alpha);
+        }"
+    }
+
+    // 百叶窗 - 从下往上
+    else if ( type === "Blinds_fragmentShader_4" )
+    {
+        ret = "\
+        varying vec2 qt_TexCoord0;          // 共享纹理坐标
+        uniform float qt_Opacity;           // 透明度
+        uniform sampler2D effectSource;     // 2D 纹理，即上面定义的 effectSource
+
+        uniform int rows;
+        uniform int cols;
+        uniform int percent;
+
+        void main()
+        {
+            vec4 color = texture2D(effectSource, qt_TexCoord0);
+
+            int xlen = 100 / cols;
+            int ylen = 100 / rows;
+
+            float p = float(percent) / 100.0;
+            int x = int(qt_TexCoord0.x * 100.0);
+            int y = int(qt_TexCoord0.y * 100.0);
+            int sx = x / xlen;
+            int sy = y / ylen;
+
+            int yOffset = 0;
+            if ( sx % 2 != 0 ) {
+                yOffset = ylen / 2;
+            }
+
+            float alpha = 0.0;
+            if ( sy * ylen <= y + yOffset && (y + yOffset) % ylen <= int(float(ylen) * p) ) {
+                alpha = 1.0;
+            }
+
+            alpha *= qt_Opacity;
+            gl_FragColor = vec4(color.rgb * alpha, alpha);
+        }"
+    }
+
+    //------------------------------------------------------------------------------
+
+    return ret
+}
